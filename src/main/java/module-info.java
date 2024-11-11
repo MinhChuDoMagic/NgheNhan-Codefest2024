a2491654ea7062687456b6a104c7c769209f5a34
minhchudomagic <chunhatminh01@gmail.com>
2024-11-11 23:23:57 +0700

    build base

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.codefest.nghenhan {
     requires javafx.controls;
     requires javafx.fxml;
+    requires socket.io.client;
+    requires okhttp3;
 
     opens org.codefest2024.nghenhan to javafx.fxml;
     exports org.codefest2024.nghenhan;

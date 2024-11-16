33c0bda719d5924b802e2431debd2239f3057fe6
MinhChuDoMagic <82219047+MinhChuDoMagic@users.noreply.github.com>
2024-11-16 11:30:12 +0700

    code base phase 1 (#3)

---
+++ src/main/java/org/codefest2024/nghenhan/utils/constant/Constants.java
@@ -0,0 +1,6 @@
+package org.codefest2024.nghenhan.utils.constant;
+
+public class Constants {
+    public static final String KEY_TEAM = "player1-xxx";
+    public static final String KEY_MAP = "b6ab900b-23af-43e9-8aa4-2eb2582d2755";
+}

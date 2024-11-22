c5c7c9d09f12bcefbfe13b624686384f5ba9bab5
minhchudomagic <chunhatminh01@gmail.com>
2024-11-22 13:55:48 +0700

    fix a* algo

--- src/main/java/org/codefest2024/nghenhan/service/caculator/data/Node.java
+++ src/main/java/org/codefest2024/nghenhan/service/caculator/data/Node.java
@@ -3,15 +3,21 @@ package org.codefest2024.nghenhan.service.caculator.data;
 import org.codefest2024.nghenhan.service.socket.data.Position;
 
 public class Node extends Position {
-    public double cost;
+    public double g;
+    public double h;
     public Node parent;
     public StringBuilder commands;
 
-    public Node(int row, int col, double cost, Node parent, StringBuilder commands) {
+    public Node(int row, int col, double g, double h, Node parent, StringBuilder commands) {
         this.row = row;
         this.col = col;
-        this.cost = cost;
+        this.g = g;
+        this.h = h;
         this.parent = parent;
         this.commands = commands == null ? new StringBuilder() : commands;
+    }
+
+    public double getF(){
+        return g+h;
     }
 }

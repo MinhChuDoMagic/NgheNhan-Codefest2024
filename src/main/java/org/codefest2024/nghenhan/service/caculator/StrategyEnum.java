fe0c6e781a978ed1d79210e34e2b2ab47fe3b7f4
minhchudomagic <chunhatminh01@gmail.com>
2024-12-03 21:43:19 +0700

    choose strategy

---
+++ src/main/java/org/codefest2024/nghenhan/service/caculator/StrategyEnum.java
@@ -0,0 +1,22 @@
+package org.codefest2024.nghenhan.service.caculator;
+
+public enum StrategyEnum {
+    FARM_STRATEGY("Farm Strategy"),
+    SEA_DIRECT_ATTACK("Sea Direct Attack"),
+    DO_NOTHING("Do Nothing");
+
+    private final String displayName;
+
+    StrategyEnum(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @Override
+    public String toString() {
+        return displayName; // To display the friendly name in the ComboBox
+    }
+}

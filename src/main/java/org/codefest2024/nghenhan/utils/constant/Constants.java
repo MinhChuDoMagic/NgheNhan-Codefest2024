4ea2e548464ab5982749ca7042eda8d8ca6364f7
minhchudomagic <chunhatminh01@gmail.com>
2024-12-11 11:01:26 +0700

    optimize farm box

--- src/main/java/org/codefest2024/nghenhan/utils/constant/Constants.java
+++ src/main/java/org/codefest2024/nghenhan/utils/constant/Constants.java
@@ -2,7 +2,8 @@ package org.codefest2024.nghenhan.utils.constant;
 
 public class Constants {
     public static String KEY_TEAM = "player1-xxx";
-    public static final String KEY_MAP = "127e85ce-325a-4542-9afd-43410963843b";
+//    public static String KEY_TEAM = "36530bff-78e3-4047-97f1-6c00edbf2f26";
+    public static final String KEY_MAP = "956e585f-357a-4b85-9b7c-8c8340b820da";
     public static final String CHARACTER_POWER = "1";
     public static final String KEY_CHILD = "_child";
 }

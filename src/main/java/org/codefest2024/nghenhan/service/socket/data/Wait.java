e51abf7773df2cee2d8c13ca4b969d97f557382c
minhchudomagic <chunhatminh01@gmail.com>
2024-12-14 11:42:27 +0700

    fix miss smashing box

--- src/main/java/org/codefest2024/nghenhan/service/socket/data/Wait.java
+++ src/main/java/org/codefest2024/nghenhan/service/socket/data/Wait.java
@@ -1,9 +1,11 @@
 package org.codefest2024.nghenhan.service.socket.data;
 
 public final class Wait extends Order{
+    public static final int REDIRECT = 400;
     public int duration;
 
-    public Wait(int duration) {
+    public Wait(int duration, boolean useChild) {
         this.duration = duration;
+        this.characterType = useChild ? USE_CHILD : null;
     }
 }
